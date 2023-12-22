f0354aa1de3293efd3b81bb2ca6af054e9b6ad77
Andrew Ahn <4andrewahn@gmail.com>
2023-12-21 16:54:24 -0800

    added git ssh and config to shell script

--- refresh_application_barplot.sh
+++ refresh_application_barplot.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 # refresh_application_barplot.sh
 
+# Log file path
+LOG_FILE="/Users/andrew/Documents/Coding/Notion-Integrations/refresh_log.log"
+
+# Start logging
+exec > >(tee -a $LOG_FILE) 2>&1
+
+echo "Starting script at $(date)"
+
+# Start SSH agent and add key
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+
+# Configure Git
+git config --global user.name "4andrewahn"
+git config --global user.email "4andrewahn@gmail.com"
+
 # Activate the virtual environment
 source /Users/andrew/Documents/Coding/Notion-Integrations/.venv/bin/activate
 
@@ -11,13 +27,18 @@ python /Users/andrew/Documents/Coding/Notion-Integrations/application_graph_scri
 cd /Users/andrew/Documents/Coding/Notion-Integrations
 
 # Add changes to new Git commit
-git add -A
+/usr/bin/git add .
 
 # Add commit message
-git commit -m "updated bar-plot"
+/usr/bin/git commit -m "updated bar-plot"
 
 # Push changes to GitHub
-git push origin main
+/usr/bin/git push origin main
 
 # Deactivate the virtual environment
 deactivate
+
+# Kill the SSH agent
+kill $SSH_AGENT_PID
+
+echo "Script completed at $(date)"
