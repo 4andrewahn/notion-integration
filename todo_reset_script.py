8607143dbe206e160f42380988d9a2f374beaab9
4andrewahn <4andrewahn@gmail.com>
2024-02-04 02:00:08 -0800

    updated barplot

--- todo_reset_script.py
+++ todo_reset_script.py
@@ -18,6 +18,11 @@ db = notion.databases.query(**{'database_id': TODO_DATABASE_ID}).get('results')
 '''
 Helper Functions
 '''
+def print_dict(input):
+    for k in input.keys():
+        print(f'\'{k}\': [{type(input[k])}] {input[k]}')
+
+
 def get_next_due_date_str(date_str, days_delta):
     # Convert string to datetime object
     date_obj = datetime.strptime(date_str, "%Y-%m-%d")
@@ -32,35 +37,47 @@ def get_next_due_date_str(date_str, days_delta):
 
 
 def update_and_reset_todo_entries(page_obj):
+    page_title = page_obj['properties']['Entry']['title'][0]['plain_text']
+    print(f'======= UPDATING Page {page_title} =======\n')
+    print(f'[Updated Total Mins Spent]')
+    print_dict(page_obj['properties']['[Updated Total Mins Spent]'])
+    print('')
+    print(f'[Updated Total #\'s Completed]')
+    print_dict(page_obj['properties']['[Updated Total #\'s Completed]'])
+    print('==============================\n')
+
     P_id = page_obj['id']
+    is_task_complete = bool(page_obj['properties']['Status']['status']['name'] == 'Done')
 
     cur_due_date_str = page_obj['properties']['Due']['date']['start']
     days_to_add = page_obj['properties']['[Repeat in # days]']['number']
     next_due_date_str = get_next_due_date_str(cur_due_date_str, days_to_add)
     
     update_data = {
-        '[Total Mins Spent]': { # Update running totals data
-            'number': page_obj['properties']['[Updated Total Mins Spent]']['formula']['number']
-        },
-        '[Total #\'s Completed]': {
-            'number': page_obj['properties']['[Updated Total #\'s Completed]']['formula']['number']
-        },
-        '[Time Started]': { # Clear Start and End timestamps
+        # Clear Start timestamps
+        '[Time Started]': { 
             'date': None
         },
-        'Status': { # Reset Status
-            'status': {
-                'name': 'Not started'
-            }
+        # Reset Status
+        'Status': { 
+            'status': {'name': 'Not started'}
         },
-        'Due': { # Increment 'Due' date by '[Repeat in # days]'
-            'date': {
-                'start': next_due_date_str
-            }
+        # Increment 'Due' date by '[Repeat in # days]'
+        'Due': { 
+            'date': {'start': next_due_date_str}
         }
     }
 
+    if is_task_complete:
+        # Clear End timestamps
+        update_data['[Time Completed]'] = {'date': None}
+        
+        # Update running totals data
+        update_data['[Total Mins Spent]'] = page_obj['properties']['[Updated Total Mins Spent]']['formula']['number']
+        update_data['[Total #\'s Completed]'] = page_obj['properties']['[Updated Total #\'s Completed]']['formula']['number']
+
     notion.pages.update(page_id=P_id, properties=update_data)
+
 
 '''
 Retrieve information and Update
